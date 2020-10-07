4cec85982c256cd80f7ce94dbbbb5b275094b463
Sahil Manhotra <sahil0730@gmail.com>
2020-10-07 18:13:44 +0530

    Updates

---
+++ precache-manifest.751c8a94968841fd4303f652df704a53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c1eb30f4c67fef09deb0480e3b6510",
+    "url": "/todo_app_using_usecontext_usereducer/index.html"
+  },
+  {
+    "revision": "ce60477ba9d10cfc67e9",
+    "url": "/todo_app_using_usecontext_usereducer/static/css/main.6762591a.chunk.css"
+  },
+  {
+    "revision": "c2122a957feb3323f3a4",
+    "url": "/todo_app_using_usecontext_usereducer/static/js/2.48e322ba.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/todo_app_using_usecontext_usereducer/static/js/2.48e322ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce60477ba9d10cfc67e9",
+    "url": "/todo_app_using_usecontext_usereducer/static/js/main.cce95d6e.chunk.js"
+  },
+  {
+    "revision": "27d9365f82475fb79119",
+    "url": "/todo_app_using_usecontext_usereducer/static/js/runtime-main.c64037d8.js"
+  }
+]);
